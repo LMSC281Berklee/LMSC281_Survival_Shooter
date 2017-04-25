68cd7a3f0093cd7097c6e31ebb62f49f30305374
Maeve Higgins <maevehiggins123@gmail.com>
2017-04-25 14:17:39 -0400

    First published edits, with michael sweet and little edits

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,5 +2,49 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+	public float speed = 6f;
+	Vector3 movement;
+	Animator anim;
+	Rigidbody playerRigidBody;
+	int floormask;
+	float camRayLength = 100f;
+
+	void Awake ()
+	{
+		LayerMask.GetMask ("Floor");
+		anim = GetComponent <Animator> ();
+		playerRigidBody = GetComponent <Rigidbody> ();
+	}
+	void FixedUpdate()
+	{
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+		Move (h, v);
+		turning ();
+		Animating (h,v);
+
+	}
+	void Move (float h , float v)
+	{ movement.Set (h, 0f, v);
+		movement = movement.normalized *speed * Time.deltaTime;
+		playerRigidBody.MovePosition (transform.position + movement );
+	}
+	void turning ()
+	{
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit floorhit;
+
+		if (Physics.Raycast (camRay, out floorhit, camRayLength, floormask)) {
+			Vector3 playerToMouse = floorhit.point - transform.position;
+			playerToMouse.y = 0f;
+			Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+			playerRigidBody.MoveRotation (newRotation);
+		} 
+	}
+	void Animating (float h, float v)
+	{
+		bool walking = h != 0 || v != 0;
+		anim.SetBool ("IsWalking", walking);
+
+}
 }
---
+++ Assets/Scripts/timerscript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timerscript : MonoBehaviour {
+
+	public int timer;
+	Text text;
+	private float timedown;
+	private float countdown;
+	private float ftimer;
+
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent <Text> ();
+		ftimer = 2000;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timedown = Time.deltaTime;
+		countdown = ftimer - timedown;
+		timer = Mathf.RoundToInt(countdown);
+		text.text = timer.ToString() ;
+	}
+}

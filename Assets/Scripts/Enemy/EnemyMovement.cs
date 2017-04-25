7216e8c22f56b87e21bfc09f3e56c57751d01a03
Maeve Higgins <maevehiggins123@gmail.com>
2017-04-25 14:30:14 -0400

    aside

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,32 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
-    Transform player;
-    //PlayerHealth playerHealth;
-    //EnemyHealth enemyHealth;
-    UnityEngine.AI.NavMeshAgent nav;
+		Transform player;               
+		PlayerHealth playerHealth;      
+		EnemyHealth enemyHealth;        
+		NavMeshAgent nav;               
 
 
-    void Awake ()
-    {
-        player = GameObject.FindGameObjectWithTag ("Player").transform;
-        //playerHealth = player.GetComponent <PlayerHealth> ();
-        //enemyHealth = GetComponent <EnemyHealth> ();
-        nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
-    }
+		void Awake ()
+		{
+			// Set up the references.
+			player = GameObject.FindGameObjectWithTag ("Player").transform;
+			playerHealth = player.GetComponent <PlayerHealth> ();
+			enemyHealth = GetComponent <EnemyHealth> ();
+			nav = GetComponent <NavMeshAgent> ();
+		}
 
 
-    void Update ()
-    {
-        //if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
-        //{
-            nav.SetDestination (player.position);
-        //}
-        //else
-        //{
-        //    nav.enabled = false;
-        //}
-    }
-}
+		void Update ()
+		{
+			
+			if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+			{
+				// ... set the destination of the nav mesh agent to the player.
+				nav.SetDestination (player.position);
+			}
+			// Otherwise...
+			else
+			{
+				// ... disable the nav mesh agent.
+				nav.enabled = false;
+			}
+		} 
+	}
